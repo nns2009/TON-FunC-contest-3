655e5920a14084bc0b1efc8d4153e69c43faa8b5
Igor Konyakhin <nns2009@yandex.ru>
2022-12-09 14:17:58 +0100

    Task 4: coded faster Curve Point multiplication in Python - it's correct

---
+++ func/4_fast.py
@@ -0,0 +1,209 @@
+import math
+
+
+prime = 2**255 - 19
+A = 486662
+B = 1
+
+def inverse_mod(v, mod):
+	return pow(v, -1, mod)
+
+def divide_mod(a, b, mod):
+	return (a * inverse_mod(b, mod) % mod)
+
+# Formulas for addition from:
+# https://www.intechopen.com/chapters/68653
+
+# Without p1==p2 case
+def add_points(p1, p2):
+	x1, y1 = p1
+	x2, y2 = p2
+	slope = divide_mod(y2 - y1, x2 - x1, prime)
+
+	x3 = (slope ** 2 - A - x1 - x2) % prime
+	y3 = (slope * (x1 - x3) - y1) % prime
+	
+	return (x3, y3)
+
+def double_point(p):
+	x, y = p
+
+	slope = divide_mod(3*x*x + 2*A*x + 1, 2*B*y, prime)
+	dx = (slope ** 2 - A - 2*x) % prime
+	dy = (slope * (x - dx) - y) % prime
+
+	return (dx, dy)
+
+# Seems correct!
+def double_x(x):
+	temp = divide_mod(
+		(3*x*x + 2*A*x + 1) ** 2,
+		4 * (x**3 + A*x**2 + x),
+		prime
+	) - A - 2*x
+	return temp % prime
+
+def add_neighbour_xs(x1, x2, base_x):
+	return divide_mod(
+		(x1 * x2 - 1) ** 2,
+		base_x * (x2 - x1) ** 2,
+		prime
+	)
+
+def triple_x(x):
+	return add_neighbour_xs(x, double_x(x), x)
+
+# Formulas for multiplication from:
+# https://www.cl.cam.ac.uk/teaching/2122/Crypto/curve25519.pdf
+# starting from page 23
+def mul_x_slow(x, n):
+	vi0 = x
+	vi1 = double_x(x)
+	process = False
+	for bi in range(255, -1, -1):
+		bit = n & (1 << bi)
+		if not process:
+			if bit:
+				process = True
+			continue
+		low = double_x(vi0)
+		mid = add_neighbour_xs(vi0, vi1, x)
+		high = double_x(vi1)
+
+		if bit:
+			vi0, vi1 = mid, high
+		else:
+			vi0, vi1 = low, mid
+	
+	return vi0
+
+
+
+def double_XZ(XZ):
+	X, Z = XZ
+	return (
+		(X**2 - Z**2) ** 2 % prime,
+		4*X*Z*(X**2 + A*X*Z + Z**2) % prime
+	)
+
+def add_neighbour_XZ(XZ0, XZ1, x):
+	X0, Z0 = XZ0
+	X1, Z1 = XZ1
+	return (
+		(X0*X1 - Z0*Z1) ** 2 % prime,
+		x*(X0*Z1 - X1*Z0) ** 2 % prime
+	)
+
+
+def mul_x_fast(x, n):
+	xz0 = (x, 1)
+	xz1 = double_XZ(xz0)
+	process = False
+	for bi in range(255, -1, -1):
+		bit = n & (1 << bi)
+		if not process:
+			if bit:
+				process = True
+			continue
+		low = double_XZ(xz0)
+		mid = add_neighbour_XZ(xz0, xz1, x);
+		high = double_XZ(xz1)
+
+		if bit:
+			xz0, xz1 = mid, high
+		else:
+			xz0, xz1 = low, mid
+
+	fX, fZ = xz0
+	res_x = divide_mod(fX, fZ, prime)
+	return res_x
+
+
+
+# ---------------------- Add test ----------------------
+print('----- Add -----')
+
+add_res = add_points(
+	(56391866308239752110494101482511933051315484376135027248208522567059122930692,
+	 17671033459111968710988296061676524036652749365424210951665329683594356030064),
+	(39028180402644761518992797890514644768585183933988208227318855598921766377692,
+	 17694324391104469229766971147677885172552105420452910290862122102896539285628)
+)
+add_correct = (
+	7769460008531208039267550090770832052561793182665100660016059978850497673345,
+	50777594312607721283178588283812137388073334114015585272572035433724485979392
+)
+
+print(add_res)
+print(add_correct)
+print('Add test: ' + ('Correct' if add_correct == add_res else 'Wrong'))
+
+
+
+
+# ---------------------- Provided multiplication test ----------------------
+print('----- Mul -----')
+
+mul_test_x = 56391866308239752110494101482511933051315484376135027248208522567059122930692
+mul_correct = 41707806908216107150933211614905026312154955484464515789593741233629885877574
+
+double_double_x = double_x(double_x(mul_test_x))
+if double_double_x != mul_correct:
+	raise f"Double double result doesn't match the expected"
+
+multiplied_x = mul_x_fast(mul_test_x, 4)
+if multiplied_x != mul_correct:
+	raise f"Multiplication result doesn't match the expected"
+
+print('Provided task test passed')
+
+
+
+
+# ---------------------- Mass multiplications test ----------------------
+
+# Base point from: https://safecurves.cr.yp.to/base.html
+base_x = 9
+base_point = (base_x, 14781619447589544791020593568409986887264606134616475288964881837755586237401)
+cur = double_point(base_point)
+ps_seq = [None, base_point, cur]
+
+mul_test_max = 10000
+for i in range(3, mul_test_max + 1):
+	cur = add_points(cur, base_point)
+	ps_seq += [cur]
+
+mul_test_show = 7
+
+print('----- Sequential addition -----')
+for i in range(1, mul_test_show):
+	print(f'{i}: seq={ps_seq[i][0]} mul={mul_x_fast(base_x, i)}')
+
+mul_correct = True
+for i in range(1, mul_test_max + 1):
+	if ps_seq[i][0] != mul_x_fast(base_x, i):
+		mul_correct = False
+		raise f'Multiplication tests failed at factor={i}'
+if mul_correct:
+	print(f'All multiplication tests passed up to factor={mul_test_max}')
+
+
+
+print('----- Specific -----')
+print(double_x(base_x))
+print(triple_x(base_x))
+
+
+
+
+print('Some info for FunC multiplication testing')
+print(400, ps_seq[400][0])
+print(500, ps_seq[500][0])
+print(900, ps_seq[900][0])
+
+print(2, ps_seq[1], ps_seq[2])
+
+
+
+# ---------------------- Separator ----------------------
+print()
