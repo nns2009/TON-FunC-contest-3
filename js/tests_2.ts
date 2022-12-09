da2f73ca303f525fd22a37b7c5d2adba405b06d6
Igor Konyakhin <nns2009@yandex.ru>
2022-12-09 17:25:42 +0100

    Task 2: inline for small performance boost
    Gas used / Score
    New: 7617448 / 5.462562288484033
    Before: 8127029 / 5.439310178475063

--- js/tests_2.ts
+++ js/tests_2.ts
@@ -6,3 +6,5 @@ import { contractLoader, cell, int, invokeGetMethod1Result, invokeGetMethodWithR
 
 let initialData = cell();
 let contract = await contractLoader('./../func/stdlib.fc', './../func/2.fc')(initialData);
+
+console.log('It compiles');
