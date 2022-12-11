ca0922a2a1c5c5cdca446e156271d37e9be43c93
Igor Konyakhin <nns2009@yandex.ru>
2022-12-11 02:47:06 +0100

    Task 5: coded binary tree sort - slightly better than merge sort
    Built-in dict sort: 22414760
    Binary tree sort: 23872411
    Merge sort: 24166924

--- js/tests_5.ts
+++ js/tests_5.ts
@@ -81,7 +81,7 @@ async function testElection(...requests: ParticipateRequest[]) {
 		console.log(`There supposed to be an error because there aren't enough participants, and there is`);
 	} else {
 		if (electResult.type !== 'success') {
-			throw new Error(`There were at least 5 participants, so try_elect should have suceeded`);
+			throw new Error(`There were at least 5 participants, so try_elect should have suceeded. exit_code=${electResult.exit_code}`);
 		}
 
 		if (electResult.actionList.length === 0) {
@@ -171,60 +171,60 @@ async function testElection(...requests: ParticipateRequest[]) {
 // 	...Array(100).fill(null).map((_, i) => ({ wc: i, addrHash: i, stake: 10, maxFactor: factor_denominator })),
 // );
 
-// await testElection(
-// 	{ wc: 5, addrHash: 5000, stake: 500, maxFactor: factor_denominator * 2 },
-// 	{ wc: 31, addrHash: 3100, stake: 300, maxFactor: factor_denominator * 2 },
-// 	{ wc: 11, addrHash: 1000, stake: 100, maxFactor: factor_denominator * 2 },
-// 	{ wc: 12, addrHash: 1000, stake: 100, maxFactor: factor_denominator * 2 },
-// 	{ wc: 13, addrHash: 1000, stake: 100, maxFactor: factor_denominator * 2 },
-// 	{ wc: 14, addrHash: 1000, stake: 100, maxFactor: factor_denominator * 2 },
-// 	{ wc: 15, addrHash: 1000, stake: 100, maxFactor: factor_denominator * 2 },
-// 	{ wc: 16, addrHash: 1000, stake: 100, maxFactor: factor_denominator * 2 },
-// 	{ wc: 17, addrHash: 1000, stake: 100, maxFactor: factor_denominator * 2 },
-// 	//{ wc: 18, addrHash: 1000, stake: 100, maxFactor: factor_denominator * 2 },
-// 	{ wc: 2, addrHash: 2000, stake: 110, maxFactor: factor_denominator * 2 },
-// 	{ wc: 2, addrHash: 2000, stake: 40, maxFactor: factor_denominator * 2 },
-// 	{ wc: 2, addrHash: 2000, stake: 50, maxFactor: factor_denominator * 2 },
-// 	{ wc: 6, addrHash: 6000, stake: 500, maxFactor: factor_denominator * 2 },
-// 	{ wc: 6, addrHash: 6000, stake: 1, maxFactor: Math.floor(factor_denominator * 3) },
-// 	// { wc: 32, addrHash: 3200, stake: 300, maxFactor: factor_denominator * 2 },
-// 	{ wc: 4, addrHash: 4000, stake: 400, maxFactor: factor_denominator * 2 },
-// 	{ wc: 100, addrHash: 10000, stake: 100000, maxFactor: factor_denominator * 100 },
-// );
-
 await testElection(
-	...(
-		Object.entries({
-		"0": {
-		  "max_factor": 192076,
-		  "stakeAmount": 1411,
-		  "sender": "0"
-		},
-		"1": {
-		  "max_factor": 377587,
-		  "stakeAmount": 6671,
-		  "sender": "1"
-		},
-		"2": {
-		  "max_factor": 270950,
-		  "stakeAmount": 8918,
-		  "sender": "2"
-		},
-		"3": {
-		  "max_factor": 302714,
-		  "stakeAmount": 431,
-		  "sender": "3"
-		},
-		"4": {
-		  "max_factor": 111062,
-		  "stakeAmount": 7158,
-		  "sender": "4"
-		}
-	}).map(([k, v]) => ({
-		wc: parseInt(k),
-		addrHash: parseInt(k) * 1000,
-		stake: v.stakeAmount,
-		maxFactor: v.max_factor,
-	}))
-	)
-)
+	{ wc: 5, addrHash: 5000, stake: 500, maxFactor: factor_denominator * 2 },
+	{ wc: 31, addrHash: 3100, stake: 300, maxFactor: factor_denominator * 2 },
+	{ wc: 11, addrHash: 1000, stake: 100, maxFactor: factor_denominator * 2 },
+	{ wc: 12, addrHash: 1000, stake: 100, maxFactor: factor_denominator * 2 },
+	{ wc: 13, addrHash: 1000, stake: 100, maxFactor: factor_denominator * 2 },
+	{ wc: 14, addrHash: 1000, stake: 100, maxFactor: factor_denominator * 2 },
+	{ wc: 15, addrHash: 1000, stake: 100, maxFactor: factor_denominator * 2 },
+	{ wc: 16, addrHash: 1000, stake: 100, maxFactor: factor_denominator * 2 },
+	{ wc: 17, addrHash: 1000, stake: 100, maxFactor: factor_denominator * 2 },
+	//{ wc: 18, addrHash: 1000, stake: 100, maxFactor: factor_denominator * 2 },
+	{ wc: 2, addrHash: 2000, stake: 110, maxFactor: factor_denominator * 2 },
+	{ wc: 2, addrHash: 2000, stake: 40, maxFactor: factor_denominator * 2 },
+	{ wc: 2, addrHash: 2000, stake: 50, maxFactor: factor_denominator * 2 },
+	{ wc: 6, addrHash: 6000, stake: 500, maxFactor: factor_denominator * 2 },
+	{ wc: 6, addrHash: 6000, stake: 1, maxFactor: Math.floor(factor_denominator * 3) },
+	// { wc: 32, addrHash: 3200, stake: 300, maxFactor: factor_denominator * 2 },
+	{ wc: 4, addrHash: 4000, stake: 400, maxFactor: factor_denominator * 2 },
+	{ wc: 100, addrHash: 10000, stake: 100000, maxFactor: factor_denominator * 100 },
+);
+
+// await testElection(
+// 	...(
+// 		Object.entries({
+// 		"0": {
+// 		  "max_factor": 192076,
+// 		  "stakeAmount": 1411,
+// 		  "sender": "0"
+// 		},
+// 		"1": {
+// 		  "max_factor": 377587,
+// 		  "stakeAmount": 6671,
+// 		  "sender": "1"
+// 		},
+// 		"2": {
+// 		  "max_factor": 270950,
+// 		  "stakeAmount": 8918,
+// 		  "sender": "2"
+// 		},
+// 		"3": {
+// 		  "max_factor": 302714,
+// 		  "stakeAmount": 431,
+// 		  "sender": "3"
+// 		},
+// 		"4": {
+// 		  "max_factor": 111062,
+// 		  "stakeAmount": 7158,
+// 		  "sender": "4"
+// 		}
+// 	}).map(([k, v]) => ({
+// 		wc: parseInt(k),
+// 		addrHash: parseInt(k) * 1000,
+// 		stake: v.stakeAmount,
+// 		maxFactor: v.max_factor,
+// 	}))
+// 	)
+// )
